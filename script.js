a69b69542fcf53e8a9eacf018bb45a4ea9c15a0c
Lucas Albuquerque <luuinhasalbuquerque@gmail.com>
2024-07-08 15:47:15 -0300

    Update questions

--- script.js
+++ script.js
@@ -11,6 +11,36 @@ const question = document.querySelector(".question");
 
     const questionsEasy = [
     {
+        question: "Qual é uma medida profilática contra Ancilostomíase/ Necatoríase (amarelão)?",
+        answers: [
+            {option: "Andar calçado", correct: true},
+            {option: "Vacina", correct: false},
+            {option: "Usar máscara", correct: false},
+            {option: "Vermifugação de pets", correct: false},
+        ],
+        difficulty: "facil"
+    },
+    {
+        question: "Qual o tratamento de zoonoses causadas por vermes?",
+        answers: [
+            {option: "Fungicida", correct: false},
+            {option: "Bactericida", correct: false},
+            {option: "Anti-viral", correct: false},
+            {option: "Vermifugação", correct: true},
+        ],
+        difficulty: "facil"
+    },
+    {
+        question: "É um grupo de risco à contaminação por malária:",
+        answers: [
+            { option: "Vegetarianos", correct: false },
+            { option: "Indígenas", correct: true },
+            { option: "Criadores de gado", correct: true },
+            { option: "Siclêmicos", correct: false },
+        ],
+        difficulty: "facil"
+    },
+    {
         question: "Um trabalhador rural se encontra doente, com pneumonia, diarreia e dores abdominais. Familiares relatam que costumava trabalhar descalço. Qual deve ser a doença?",
         answers: [
             { option: "Amarelão", correct: true },
@@ -21,34 +51,84 @@ const question = document.querySelector(".question");
         difficulty: "facil" 
     },
     {
-        question: "Qual é um sinal típico de Amarelão?",
+        question: "Qual dos ambientes a seguir é marcado pela recorrência de casos de Doença de Chagas?",
         answers: [
-            { option: "Prurido anal", correct: false },
-            { option: "Olhos vermelhos", correct: false },
-            { option: "Paralisia", correct: false },
-            { option: "Pneumonia", correct: true },
+            { option: "Casa de taipa (com paredes de barro)", correct: true },
+            { option: "Áreas litorâneas", correct: false },
+            { option: "Áreas não saneadas", correct: false },
+            { option: "Jardins domésticos", correct: false },
         ],
         difficulty: "facil" 
     },
     {
-        question: "O que é correto sobre as arboviroses?",
+        question: "Qual o vetor mais comum da elefantíase?",
         answers: [
-            { option: "São transmitidas apenas por mosquitos", correct: false },
-            { option: "São transmitidas por macacos", correct: false },
-            { option: "São contagiosas", correct: false },
-            { option: "Podem ser transmitidas por carrapatos", correct: true },
+            { option: "Muriçocas", correct: true },
+            { option: "Rãs", correct: false },
+            { option: "Macacos", correct: false },
+            { option: "Cães e gatos", correct: false },
         ],
         difficulty: "facil" 
     },
     {
-        question: "Sobre zoonoses, pode-se afirmar:",
+        question: "Qual é uma medida profilática eficiente contra zoonoses causadas por vermes?",
         answers: [
-            { option: "A febre maculosa é transmitida por um inseto", correct: true },
-            { option: "A filariose é transmitida por um protozoário", correct: false },
-            { option: "A leishmaniose é transmitida pelo flebótomo", correct: true },
-            { option: "O bicho geográfico pode ser causado por vírus", correct: false },
+            { option: "Saneamento básico", correct: true },
+            { option: "Vacinação", correct: false },
+            { option: "Comer apenas vegetais e evitar carnes", correct: false },
+            { option: "Suplementação de Magnésio", correct: false },
         ],
-        difficulty: "facil"
+        difficulty: "facil" 
+    },
+    {
+        question: "Como se prevenir da teníase?",
+        answers: [
+            { option: "Lavando alimentos crus antes de comer", correct: true },
+            { option: "Andando calçado", correct: false },
+            { option: "Usando máscara", correct: false },
+            { option: "Usando soro", correct: false },
+        ],
+        difficulty: "facil" 
+    },
+    {
+        question: "Sobre a elefantíase pode-se afirmar:",
+        answers: [
+            { option: "Transmitida pelos mesmos vetores da dengue", correct: false },
+            { option: "Transmitida por capivaras", correct: false },
+            { option: "Causada por bactéria", correct: false },
+            { option: "Causada por um verme", correct: true },
+        ],
+        difficulty: "facil" 
+    },
+    {
+        question: "Sobre arboviroses:",
+        answers: [
+            { option: "Macacos servem como vetores", correct: false },
+            { option: "Macacos servem como sentinelas de controle sanitário da doença", correct: true },
+            { option: "Hidroxicloroquina não é uma profilaxia eficaz para malária", correct: false },
+            { option: "Malária é uma doença viral", correct: false },
+        ],
+        difficulty: "facil" 
+    },
+    {
+        question: "Trata-se de um sintoma típico da raiva:",
+        answers: [
+            { option: "Aversão à água e dor ao ingerí-la", correct: true },
+            { option: "Anemia", correct: false },
+            { option: "Cegueira repentina", correct: false },
+            { option: "Náusea", correct: false },
+        ],
+        difficulty: "facil" 
+    },
+    {
+        question: "Sobre a raiva, o que não é correto?",
+        answers: [
+            { option: "Hidrofobia é um sinal terminal da doença", correct: false },
+            { option: "O vírus procura se replicar no sistema nervoso", correct: false },
+            { option: "Soros anti rábicos e vacinas podem ser usados indistintamente para o tratamento", correct: true },
+            { option: "Agressividade é um sintoma típico", correct: false },
+        ],
+        difficulty: "facil" 
     },
     {
         question: "O agente etiológico da raiva é:",
@@ -91,16 +171,6 @@ const question = document.querySelector(".question");
         difficulty: "facil"
     },
     {
-        question: "Sobre a febre maculosa pode-se afirmar:",
-        answers: [
-            { option: "Ocorre sobretudo em áreas mais urbanizadas", correct: false },
-            { option: "O carrapato vetor pode parasitar capivaras e cutias", correct: true },
-            { option: "Não transmissível para humanos", correct: false },
-            { option: "O tratamento é antiviral", correct: false },
-        ],
-        difficulty: "facil"
-    },
-    {
         question: "O agente etiológico do Carbúnculo é:",
         answers: [
             { option: "Um vírus", correct: false },
@@ -111,12 +181,62 @@ const question = document.querySelector(".question");
         difficulty: "facil"
     },
     {
+        question: "O agente etiológico da Leptospirose é:",
+        answers: [
+            { option: "Um vírus", correct: false },
+            { option: "Uma bactéria", correct: true },
+            { option: "Um protozoário", correct: false },
+            { option: "Um verme", correct: false },
+       ],
+        difficulty: "facil"
+    },
+    {
+        question: "O agente etiológico da Toxoplasmose é:",
+        answers: [
+            { option: "Um vírus", correct: false },
+            { option: "Uma bactéria", correct: false },
+            { option: "Um protozoário", correct: true },
+            { option: "Um verme", correct: false },
+       ],
+        difficulty: "facil"
+    },
+    {
         question: "Sobre o Antraz (carbúnculo) pode-se afirmar:",
         answers: [
             { option: "Seu ciclo de transmissão pode envolver bovinos e mosquitos contaminados", correct: true },
             { option: "A contaminação é exclusivamente pelo solo contaminado", correct: false },
-            { option: "Saneamento básico é uma forma eficaz de prevenção", correct: true },
+            { option: "Saneamento básico é uma forma eficaz de prevenção", correct: false },
             { option: "A transmissão é exclusivamente feco-oral", correct: false },
+        ],
+        difficulty: "facil"
+    },
+    {
+        question: "Em épocas de muitas chuvas com enchentes, espera-se um aumento do número de casos de:",
+        answers: [
+            { option: "Leptospirose e dengue", correct: true },
+            { option: "Cólera e gonorreia", correct: false },
+            { option: "Esquistossomose e tifo", correct: false },
+            { option: "Ascaridíase e toxoplasmose", correct: false },
+        ],
+        difficulty: "facil"
+    },
+    {
+        question: "Sobre a leptospirose é correto afirmar:",
+        answers: [
+            { option: "Permitir que seu pet tenha contato com a urina de outros pets é uma forma de protegê-lo", correct: false },
+            { option: "A bactéria causadora precisa ser ingerida para causar a doença", correct: false },
+            { option: "A bactéria causadora vive na superfície de ratos (reservatórios naturais desse agente etiológico)", correct: false },
+            { option: "Urina escura é um sintoma da doença", correct: true },
+        ],
+        difficulty: "facil"
+    },
+    {
+        question: "No Brasil, qual a tripanossomíase mais comum?",
+        answers: [
+            { option: "Doença de Chagas", correct: true },
+            { option: "Doença do Sono", correct: false },
+            { option: "Malária", correct: false },
+            { option: "Leptospirose", correct: false },
         ],
         difficulty: "facil"
     },
@@ -142,6 +262,127 @@ const question = document.querySelector(".question");
             { option: "Oxiurose", correct: true },
         ],
         difficulty: "medio" 
+    },
+    {
+        question: "Um paciente come carne suína de matadouros ilegais e apresenta dor abdominal, qual pode ser a causa?",
+        answers: [
+            { option: "Amarelão", correct: false },
+            { option: "Teníase", correct: true },
+            { option: "Dengue", correct: false },
+            { option: "Carbúnculo", correct: false },
+        ],
+        difficulty: "medio" 
+    },
+    {
+        question: "Qual o primeiro sinal típico de que se foi picado pelo barbeiro?",
+        answers: [
+            { option: "Febre repentina", correct: false },
+            { option: "Náusea repentina", correct: false },
+            { option: "Coceira e vermelhidão nas pálpebras", correct: true },
+            { option: "Prurido anal", correct: false },
+        ],
+        difficulty: "medio" 
+    },
+    {
+        question: "O que é correto sobre as arboviroses?",
+        answers: [
+            { option: "São transmitidas apenas por mosquitos", correct: false },
+            { option: "São transmitidas por macacos", correct: false },
+            { option: "São contagiosas", correct: false },
+            { option: "Podem ser transmitidas por carrapatos", correct: true },
+        ],
+        difficulty: "medio" 
+    },
+    {
+        question: "Os ciclos silvestres de transmissão da Leishmaniose costumam envolver:",
+        answers: [
+            { option: "Raposas e morcegos", correct: true },
+            { option: "Ratos e gatos", correct: false },
+            { option: "Peixes e algas", correct: false },
+            { option: "Apenas cães", correct: false },
+        ],
+        difficulty: "medio" 
+    },
+    {
+        question: "Qual é um sinal típico de Malária?",
+        answers: [
+            { option: "Diarreia", correct: false },
+            { option: "Febre alta e intermitente", correct: true },
+            { option: "Sonolência", correct: false },
+            { option: "Pneumonia", correct: false },
+        ],
+        difficulty: "medio" 
+    },
+    {
+        question: "Marque a alternativa verdadeira sobre a dengue:",
+        answers: [
+            { option: "Existem dois gêneros de mosquitos vetores", correct: false },
+            { option: "Possui transmissão feco-ora", correct: false },
+            { option: "Dengue hemorrágica só acontece em imunossuprimidos", correct: false },
+            { option: "Febre alta de início repentino é um sinal típico", correct: true },
+        ],
+        difficulty: "medio" 
+    },
+    
+    {
+        question: "Sobre zoonoses, pode-se afirmar:",
+        answers: [
+            { option: "A febre maculosa é transmitida por um inseto", correct: false },
+            { option: "A filariose é transmitida por um protozoário", correct: false },
+            { option: "A leishmaniose é transmitida pelo flebótomo", correct: true },
+            { option: "O bicho geográfico pode ser causado por vírus", correct: false },
+        ],
+        difficulty: "medio"
+    },
+    {
+        question: "Marque a alternativa verdadeira:",
+        answers: [
+            { option: "Verminoses podem causar pneumonia e meningite", correct: true },
+            { option: "Comer carne de boi mal cozida representa risco de cisticercose", correct: false },
+            { option: "Amebíase é uma zoonose comum em grandes cidades", correct: false },
+            { option: "Toxoplasmose é causada por um protozoário que parasita apenas humanos e gatos", correct: false },
+        ],
+        difficulty: "medio"
+    },
+    {
+        question: "Sobre a febre maculosa pode-se afirmar:",
+        answers: [
+            { option: "Ocorre sobretudo em áreas mais urbanizadas", correct: false },
+            { option: "O carrapato vetor pode parasitar capivaras e cutias", correct: true },
+            { option: "Não transmissível para humanos", correct: false },
+            { option: "O tratamento é antiviral", correct: false },
+        ],
+        difficulty: "medio"
+    },
+    {
+        question: "Qual o nome do agente causador da Doença de Chagas?",
+        answers: [
+            { option: "Trypanosoma Cruzi", correct: true },
+            { option: "Wuchereria bancrofti", correct: false },
+            { option: "Rickettsia spp.", correct: false },
+            { option: "Plasmodium Falciparum", correct: false },
+        ],
+        difficulty: "medio"
+    },
+    {
+        question: "O agente causador da malária se replica principalmente:",
+        answers: [
+            { option: "Nos eritrócitos e no fígado", correct: true },
+            { option: "No baço", correct: false },
+            { option: "No coração", correct: false },
+            { option: "No sistema nervoso central", correct: false },
+        ],
+        difficulty: "medio"
+    },
+    {
+        question: "Qual não é uma forma de contaminação por Toxoplasmose?",
+        answers: [
+            { option: "Comer carne de bovinos contaminados", correct: false },
+            { option: "Contato com fezes de felinos contaminados", correct: false },
+            { option: "Contato com água e alimentos contaminados pelas fezes de felinos com a doença", correct: false },
+            { option: "Pela urina de ratos contaminados", correct: true },
+        ],
+        difficulty: "medio"
     },
     {
         question: "Qual dos locais a seguir implica maior risco de contaminação com Leishmaniose?",
@@ -172,16 +413,6 @@ const question = document.querySelector(".question");
             { option: "Podem ser transmitidas por carrapatos", correct: true },
         ],
         difficulty: "medio" 
-    },
-    {
-        question: "Sobre a malária pode-se afirmar:",
-        answers: [
-            { option: "Possui humanos como o hospedeiro definitivo ou final", correct: false },
-            { option: "O protozoário causador da doença é monoxeno", correct: false },
-            { option: "Transmitida por machos e fêmeas do mosquito-prego", correct: false },
-            { option: "Caracterizada por ciclos de febre alta seguidos de apirexia", correct: true },
-        ],
-        difficulty: "medio"
     },
 ]
 
@@ -231,7 +462,7 @@ const question = document.querySelector(".question");
         question: "Sobre a malária, pode-se afirmar:",
         answers: [
             { option: "O agente etiológico mais virulento é o Plasmodium malariae", correct: false },
-            { option: "Plasmodium Vivax causa febre terçã benigna", correct: true },
+            { option: "Plasmodium Vivax causa febre terçã benigna", correct: false },
             { option: "Plasmodium Falciparum é o agente etiológico mais virulento", correct: true },
             { option: "P. Falciparum causa febre quartã maligna", correct: false },
         ],
@@ -284,7 +515,7 @@ const question = document.querySelector(".question");
         question: "Sobre a teníase pode-se afirmar:",
         answers: [
             { option: "Taenia solium possui um rostro de ganchos quitinosos para se prender à parede do intestino delgado", correct: true },
-            { option: "Taenia saginata parasita bovinos e suínos", correct: true },
+            { option: "Taenia saginata parasita bovinos e suínos", correct: false },
             { option: "Taenia spp. tem sistema digestório completo", correct: false },
             { option: "Taenia spp. fazem fermentação alcoólica no intestino delgado", correct: false },
         ],
